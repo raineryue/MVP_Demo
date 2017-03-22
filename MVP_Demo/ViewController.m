5c402b3d065520678364f837008f88519a9ca2b7
Rainer <it_jinlei.yue@163.com>
2017-03-22 15:59:27 +0800

    Initial Commit

---
+++ MVP_Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MVP_Demo
+//
+//  Created by Rainer on 2017/3/22.
+//  Copyright © 2017年 Rainer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ MVP_Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MVP_Demo
+//
+//  Created by Rainer on 2017/3/22.
+//  Copyright © 2017年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
